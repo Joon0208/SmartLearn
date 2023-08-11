a53178575f054ba12f0076f615a011e7b3253efe
Joon0208 <yjoon0208@gmail.com>
2023-08-11 12:43:28 +0800

    Remove unnecessary picture

--- SmartLearn/__init__.py
+++ SmartLearn/__init__.py
@@ -1,23 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, Response, jsonify
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, UserMixin, current_user
 from Forms import *
 import shelve, base64, os
-from facial_recognition import gen_frames
 import cv2 as cv
 import mediapipe as mp
 import time
 import numpy as np
-from flask_socketio import SocketIO
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, UserMixin, current_user
+
 from datetime import datetime
-
-
-#Login mail@mail.com
-#password 12345
-
-#Login2 nomail@mail.com
-#password2 54321
-
+import math
+import face_recognition
+import io
 
 app = Flask(__name__)
 app.secret_key = 'hi'
@@ -32,6 +27,8 @@ login_manager = LoginManager()
 login_manager.login_view = 'login'  # Update with your login route
 login_manager.init_app(app)
 
+from flask_login import UserMixin
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
@@ -39,8 +36,6 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(150))
     date_joined = db.Column(db.Date, default=datetime.utcnow)
     role = db.Column(db.String(20))
-
-from flask_login import UserMixin
 
 class Exam_Attempt(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -53,26 +48,14 @@ class Exam_Attempt(db.Model, UserMixin):
         self.non_center_eye_count = non_center_eye_count
         self.time_taken = time_taken
 
+class ImageModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(255), nullable=False)
+    image_data = db.Column(db.LargeBinary, nullable=False)
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-# Define the Users model
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     non_center_eye_count = db.Column(db.Integer, nullable=False)
-#     elapsed_time_seconds = db.Column(db.Float, nullable=False)
-
-#     def __init__(self, name, non_center_eye_count, elapsed_time_seconds):
-#         self.name = name
-#         self.non_center_eye_count = non_center_eye_count
-#         self.elapsed_time_seconds = elapsed_time_seconds
-
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
 
 @app.route('/')
 def home():
@@ -152,7 +135,7 @@ def signup():
                 login_user(new_user, remember=True)
 
                 flash("Account created", category='success')
-                return redirect(url_for('login'))  # Update with your login route
+                return redirect(url_for('face_registration', name = name))  # Update with your login route
 
         except Exception as e:
             flash('An error occurred while creating the account', category='error')
@@ -199,34 +182,39 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route('/face_registration', methods=['GET', 'POST'])
+@app.route('/face_registration', methods=['GET','POST'])
 def face_registration():
     if request.method == 'POST':
-        name = request.form.get('name')
-        face_image = request.form.get('face_image')
+        user_name = request.args.get('name')
+        if user_name is None:
+            flash('User name not found', category='error')
+            return redirect(url_for('signup'))
 
-        # Convert the base64 image data to bytes
+        face_image = request.form.get('face_image')
         face_image = face_image.split(',')[1].encode()
-        # Decode the image and save it to a file
         image_data = base64.b64decode(face_image)
-        # Generate a unique filename using the current timestamp
-        image_filename = f'{name}.jpg'
-        # Create a directory for the user if it doesn't exist
-        user_dir = os.path.join('faces', image_filename)
-        # Save the captured image under the user's directory
-        image_path = os.path.join(user_dir)
+
+        # Create an instance of your ImageModel and set the values
+        image_instance = ImageModel(user_name=user_name, image_data=image_data)
+
+        # Add the instance to the session and commit the changes
+        db.session.add(image_instance)
+        db.session.commit()
+
+        # Generate a unique filename using the user's name
+        image_filename = f'{user_name}.jpg'
+
+        # Save the captured image directly under the 'faces' directory
+        image_path = os.path.join('faces', image_filename)
         with open(image_path, 'wb') as f:
             f.write(image_data)
 
-        # Render the template with the success message
-        return render_template('face_registration.html', detection_status=True, face_image=image_path,
-                               success_message='Image saved successfully.')
+        return redirect(url_for('login'))
 
     return render_template('face_registration.html', detection_status=False)
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 
 @app.route('/eye_tracking_video')
 def eye_tracking_video():
@@ -235,10 +223,6 @@ def eye_tracking_video():
 @app.route('/eye_tracking')
 def eye_tracking():
     return render_template('eye_tracking.html')
-
-@app.route('/exam_monitoring')
-def exam_monitoring():
-    return render_template('exam_monitoring.html')
 
 @app.route('/student_homepage')
 def student_homepage():
@@ -300,7 +284,148 @@ def stop_camera():
 
     return jsonify({'message': 'Camera stopped successfully'})
 
-# eye_tracking function
+
+# Facial Recognition Function
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+from flask import redirect, url_for
+
+@app.route('/face_lock')
+def face_lock():
+    global fr
+
+    if fr is None:
+        fr = FaceRecognition()
+    print(fr.face_names)
+    detected_users = [name.split(' ')[0] for name in fr.face_names]
+    current_user_name = current_user.name  # Assuming 'current_user' is a User object from Flask-Login
+    print("Detected Users:", detected_users)
+    print("Current User Name:", current_user_name)
+
+    if current_user in detected_users:
+        allow_unlock = True
+        message = "User detected. Click the button to unlock."
+        print('Authenticated')
+    else:
+        allow_unlock = False
+        print('Wrong User')
+        message = "Wrong user detected."
+
+    return render_template('face_lock.html', allow_unlock=allow_unlock, message=message)
+
+
+fr = None  # FaceRecognition instance
+
+def gen_frames():
+    global fr
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            # Perform face recognition on the frame
+            if fr is None:
+                fr = FaceRecognition()
+            frame = fr.run_recognition(frame)
+
+            ret, buffer = cv.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+def face_confidence(face_distance, face_match_threshold=0.6):
+    range = (1.0 - face_match_threshold)
+    linear_val = (1.0 - face_distance) / (range * 2.0)
+
+    if face_distance > face_match_threshold:
+        return str(round(linear_val * 100, 2)) + '%'
+    else:
+        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
+        return str(round(value, 2)) + '%'
+
+class FaceRecognition:
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    known_face_encodings = []
+    known_face_names = []
+    process_current_frame = True
+
+    def __init__(self):
+        self.encode_faces()
+
+    def encode_faces(self):
+        # Assuming you have a SQLAlchemy model named ImageModel
+        with app.app_context():
+            images = ImageModel.query.all()
+
+        for image in images:
+            face_image = face_recognition.load_image_file(io.BytesIO(image.image_data))
+            face_encoding = face_recognition.face_encodings(face_image)[0]
+
+            self.known_face_encodings.append(face_encoding)
+            self.known_face_names.append(image.user_name)
+
+        print(self.known_face_names)
+
+    def run_recognition(self, frame):
+        # Perform face recognition on the provided frame
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # Find all the faces and face encodings in the current frame of video
+        self.face_locations = face_recognition.face_locations(rgb_small_frame)
+        self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+
+        self.face_names = []
+        self.detected_user = None
+        for face_encoding in self.face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+            name = "Unknown"
+            confidence = '???'
+
+            # Calculate the shortest distance to face
+            face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = self.known_face_names[best_match_index]
+                confidence = face_confidence(face_distances[best_match_index])
+
+            name = name.split(".")[0]
+            self.face_names.append(f'{name} ({confidence})')
+            self.detected_user = name
+            print(name)
+
+
+        # Display the results
+        for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Create the frame with the name
+            cv.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv.FILLED)
+            cv.putText(frame, name, (left + 6, bottom - 6), cv.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+        return frame
+
+
+
+
+
+
+# Exam Monitoring Function
 
 frame_counter = 0
 FONTS = cv.FONT_HERSHEY_COMPLEX
